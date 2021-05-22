fc4397e610f2c11e20613df1c78c86c7ed83f4a7
Monica <monicabee358@gmail.com>
2021-05-22 22:58:43 +0800

    Added Edit feature for edit username and email

--- controller/profileController.js
+++ controller/profileController.js
@@ -32,114 +32,351 @@ exports.editProfile = async (req, res) => {
   // console.log(req.body.uuid);
   //console.log(req.body.email);
 
-  /*   if (req.body.user != req.body.username && req.body.email != req.body.email_address){
-        console.log(req.body.username);
-        console.log(req.body.email_address);
-        console.log("I want to change both!");
-    }else if(req.body.email != req.body.email_address){
-        console.log(req.body.username);
-        console.log("Change my email address ONLY");
-    }else if (req.body.user != req.body.username){
-        console.log(req.body.email_address);
-        console.log("Change my Username ONLY");
-    } */
-
-  let data0 = await account.model
-    .findOne({
-      where: {
-        username: req.body.username,
-      },
-    })
-    .then(function (userAuth) {
-      if (userAuth) {
-        //username to be changed exists
-        expense.model
-          .findAll({
-            where: {
-              uuid: req.body.uuid,
-            },
-          })
-          .then((user2) => {
-            if (!user2) {
-              console.log("no1");
-            } else {
-              console.log("yes1");
-              account.model
-                .findOne({
-                  where: {
-                    uuid: req.body.uuid,
-                  },
-                })
-                .then((user3) => {
-                  if (!user3) {
-                    console.log("no11");
-                  } else {
-                    console.log("yes22");
-
-                    req.session.user1 = user3;
-                    req.session.expense1 = user2;
-                    req.session.successful = undefined;
-                    req.session.error = "Username is already taken!";
-                    res.redirect("/home");
-                  }
-                });
-            }
-          });
-      } else {
-        //doesnt exist
-        account.model
-          .update(
-            {
-              username: req.body.username,
-              email_address: req.body.email_address, //
-            },
-            {
+  if (
+    req.body.user != req.body.username &&
+    req.body.email != req.body.email_address
+  ) {
+    console.log(req.body.username);
+    console.log(req.body.email_address);
+    console.log("I want to change both!");
+    account.model
+      .findOne({
+        where: {
+          //email_address: req.body.email_address,
+          username: req.body.username,
+        },
+      })
+      .then(function (isUserExists) {
+        if (isUserExists) {
+          //username to be changed exists
+          expense.model
+            .findAll({
               where: {
-                username: req.body.user,
+                uuid: req.body.uuid,
               },
-            }
-          )
-          .then((user) => {
-            if (!user) {
-              console.log("Unable to update your profile");
-            } else {
-              console.log("profile updated");
+            })
+            .then((user2) => {
+              if (!user2) {
+                console.log("no1");
+              } else {
+                console.log("yes1");
+                account.model
+                  .findOne({
+                    where: {
+                      uuid: req.body.uuid,
+                    },
+                  })
+                  .then((user3) => {
+                    if (!user3) {
+                      console.log("no11");
+                    } else {
+                      console.log("yes22");
+                      req.session.user1 = user3;
+                      req.session.expense1 = user2;
+                      req.session.successful = undefined;
+                      req.session.error = "Username is already taken!";
+                      res.redirect("/home");
+                    }
+                  });
+              }
+            });
+        } else {
+          //new username doesnt exist
+          account.model
+            .findOne({
+              where: {
+                email_address: req.body.email_address,
+              },
+            })
+            .then(function (isEmailExists) {
+              if (isEmailExists) {
+                //Email Address to be changed exists
+                expense.model
+                  .findAll({
+                    where: {
+                      uuid: req.body.uuid,
+                    },
+                  })
+                  .then((user2) => {
+                    if (!user2) {
+                      console.log("no1");
+                    } else {
+                      console.log("yes1");
+                      account.model
+                        .findOne({
+                          where: {
+                            uuid: req.body.uuid,
+                          },
+                        })
+                        .then((user3) => {
+                          if (!user3) {
+                            console.log("no11");
+                          } else {
+                            console.log("yes22");
+                            req.session.user1 = user3;
+                            req.session.expense1 = user2;
+                            req.session.successful = undefined;
+                            req.session.error =
+                              "Email Address is already taken!";
+                            res.redirect("/home");
+                          }
+                        });
+                    }
+                  });
+              } else {
+                //doesnt exist
+                account.model
+                  .update(
+                    {
+                      username: req.body.username,
+                      email_address: req.body.email_address,
+                    },
+                    {
+                      where: {
+                        uuid: req.body.uuid,
+                      },
+                    }
+                  )
+                  .then((user) => {
+                    if (!user) {
+                      console.log("Unable to update your profile");
+                    } else {
+                      console.log("profile updated (email & username)");
 
-              expense.model
-                .findAll({
-                  where: {
-                    uuid: req.body.uuid,
-                  },
-                })
-                .then((user2) => {
-                  if (!user2) {
-                    console.log("no");
-                  } else {
-                    console.log("yes");
-                    account.model
-                      .findOne({
-                        where: {
-                          uuid: req.body.uuid,
-                        },
-                      })
-                      .then((user3) => {
-                        if (!user3) {
-                          console.log("no1");
-                        } else {
-                          console.log("yes2");
+                      expense.model
+                        .findAll({
+                          where: {
+                            uuid: req.body.uuid,
+                          },
+                        })
+                        .then((user2) => {
+                          if (!user2) {
+                            console.log("no - email & user");
+                          } else {
+                            console.log("yes - email & user");
+                            account.model
+                              .findOne({
+                                where: {
+                                  uuid: req.body.uuid,
+                                },
+                              })
+                              .then((user3) => {
+                                if (!user3) {
+                                  console.log("no1 - email & user");
+                                } else {
+                                  console.log("yes2 - email & user");
+                                  req.session.user1 = user3;
+                                  req.session.expense1 = user2;
+                                  req.session.error = undefined;
+                                  req.session.successful =
+                                    "Email Address and Username successfully changed!";
+                                  res.redirect("/home");
+                                }
+                              });
+                          }
+                        });
+                    }
+                  });
+              }
+            });
+        }
+      });
+  } else if (req.body.email != req.body.email_address) {
+    console.log(req.body.username);
+    console.log("Change my email address ONLY");
+    let data0 = await account.model
+      .findOne({
+        where: {
+          email_address: req.body.email_address,
+        },
+      })
+      .then(function (userAuth) {
+        if (userAuth) {
+          //username to be changed exists
+          expense.model
+            .findAll({
+              where: {
+                uuid: req.body.uuid,
+              },
+            })
+            .then((user2) => {
+              if (!user2) {
+                console.log("no1");
+              } else {
+                console.log("yes1");
+                account.model
+                  .findOne({
+                    where: {
+                      uuid: req.body.uuid,
+                    },
+                  })
+                  .then((user3) => {
+                    if (!user3) {
+                      console.log("no11");
+                    } else {
+                      console.log("yes22");
+                      req.session.user1 = user3;
+                      req.session.expense1 = user2;
+                      req.session.successful = undefined;
+                      req.session.error = "Email Address is already taken!";
+                      res.redirect("/home");
+                    }
+                  });
+              }
+            });
+        } else {
+          //doesnt exist
+          account.model
+            .update(
+              {
+                email_address: req.body.email_address,
+              },
+              {
+                where: {
+                  username: req.body.user,
+                },
+              }
+            )
+            .then((user) => {
+              if (!user) {
+                console.log("Unable to update your profile");
+              } else {
+                console.log("profile updated (email)");
 
-                          req.session.user1 = user3;
-                          req.session.expense1 = user2;
-                          req.session.error = undefined;
-                          req.session.successful =
-                            "Username successfully changed!";
-                          res.redirect("/home");
-                        }
-                      });
-                  }
-                });
-            }
-          });
-      }
-    });
+                expense.model
+                  .findAll({
+                    where: {
+                      uuid: req.body.uuid,
+                    },
+                  })
+                  .then((user2) => {
+                    if (!user2) {
+                      console.log("no - email");
+                    } else {
+                      console.log("yes - email");
+                      account.model
+                        .findOne({
+                          where: {
+                            uuid: req.body.uuid,
+                          },
+                        })
+                        .then((user3) => {
+                          if (!user3) {
+                            console.log("no1 - email");
+                          } else {
+                            console.log("yes2 - email");
+                            req.session.user1 = user3;
+                            req.session.expense1 = user2;
+                            req.session.error = undefined;
+                            req.session.successful =
+                              "Email Address successfully changed!";
+                            res.redirect("/home");
+                          }
+                        });
+                    }
+                  });
+              }
+            });
+        }
+      });
+  } else if (req.body.user != req.body.username) {
+    console.log(req.body.email_address);
+    console.log("Change my Username ONLY");
+    let data0 = await account.model
+      .findOne({
+        where: {
+          username: req.body.username,
+        },
+      })
+      .then(function (userAuth) {
+        if (userAuth) {
+          //username to be changed exists
+          expense.model
+            .findAll({
+              where: {
+                uuid: req.body.uuid,
+              },
+            })
+            .then((user2) => {
+              if (!user2) {
+                console.log("no1 - username");
+              } else {
+                console.log("yes1 - username");
+                account.model
+                  .findOne({
+                    where: {
+                      uuid: req.body.uuid,
+                    },
+                  })
+                  .then((user3) => {
+                    if (!user3) {
+                      console.log("no11 - username");
+                    } else {
+                      console.log("yes22 - username");
+                      req.session.user1 = user3;
+                      req.session.expense1 = user2;
+                      req.session.successful = undefined;
+                      req.session.error = "Username is already taken!";
+                      res.redirect("/home");
+                    }
+                  });
+              }
+            });
+        } else {
+          //doesnt exist
+          account.model
+            .update(
+              {
+                username: req.body.username,
+                //email_address: req.body.email_address,
+              },
+              {
+                where: {
+                  username: req.body.user,
+                },
+              }
+            )
+            .then((user) => {
+              if (!user) {
+                console.log("Unable to update your profile");
+              } else {
+                console.log("profile updated (username)");
+
+                expense.model
+                  .findAll({
+                    where: {
+                      uuid: req.body.uuid,
+                    },
+                  })
+                  .then((user2) => {
+                    if (!user2) {
+                      console.log("no - username");
+                    } else {
+                      console.log("yes - username ");
+                      account.model
+                        .findOne({
+                          where: {
+                            uuid: req.body.uuid,
+                          },
+                        })
+                        .then((user3) => {
+                          if (!user3) {
+                            console.log("no1 - username");
+                          } else {
+                            console.log("yes2 - username");
+                            req.session.user1 = user3;
+                            req.session.expense1 = user2;
+                            req.session.error = undefined;
+                            req.session.successful =
+                              "Username successfully changed!";
+                            res.redirect("/home");
+                          }
+                        });
+                    }
+                  });
+              }
+            });
+        }
+      });
+  }
 };
